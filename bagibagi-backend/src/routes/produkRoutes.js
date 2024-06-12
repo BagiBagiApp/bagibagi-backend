58d7183c1f9efc3a3ba71f5f20f69f030cfb9d8f
jinggamentari <vladijm@gmail.com>
2024-06-12 14:01:28 +0700

    feat: done product search query

--- bagibagi-backend/src/routes/produkRoutes.js
+++ bagibagi-backend/src/routes/produkRoutes.js
@@ -13,33 +13,51 @@ const multer = Multer({
     fileSize: 5 * 1024 * 1024
 })
 
-//GET ALL PRODUCTS
-route.get('/allProducts',authenticateToken, async(req,res) => {
+//GET ALL PRODUCTS + SEARCH QUERY
+route.get('/allProducts', authenticateToken, async (req, res) => {
     try {
-        const data = await supabase.from('barang').select('*');
-        return res.status(200).json({data: data});
+        // const data = await supabase.from('barang').select('*');
+        const { nama_produk } = req.query;
+
+        const { data } = await supabase.from('barang').select('*');
+
+        let filteredData = data;
+
+        if (nama_produk) {
+            const searchQuery = decodeURIComponent(nama_produk).toLowerCase();
+            filteredData = data.filter(product =>
+                product.nama_produk.toLowerCase().includes(searchQuery)
+            );
+        }
+
+        if (filteredData.length > 0) {
+            return res.status(200).json({ data: filteredData });
+        } else {
+            return res.status(200).json({ data: [], message: 'No products found' });
+        }
+
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
 });
 
 //GET PRODUCT'S DETAILS
-route.get('/:id', authenticateToken, async(req,res) => {
+route.get('/:id', authenticateToken, async (req, res) => {
     try {
         const { id } = req.params;
         const details = await supabase.from('barang').select('*').eq('id', id);
         const ownersName = await supabase.from('users').select('username').eq('id', details.data[0].pemilik);
         details.data[0].pemilik = ownersName.data[0].username;
-        return res.status(200).json({data: details});
+        return res.status(200).json({ data: details });
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
 });
 
 //CREATE A PRODUCT
-route.post('/upload', authenticateToken, multer.single('foto'), imgUpload.uploadToGcs, async(req,res) => {
+route.post('/upload', authenticateToken, multer.single('foto'), imgUpload.uploadToGcs, async (req, res) => {
     try {
-        const {nama_produk, desc, kategori, qty, status, years_of_usage} = req.body;
+        const { nama_produk, desc, kategori, qty, status, years_of_usage } = req.body;
         var imageUrl = ''
 
         if (req.file && req.file.cloudStoragePublicUrl) {
@@ -65,21 +83,21 @@ route.post('/upload', authenticateToken, multer.single('foto'), imgUpload.upload
 });
 
 //UPDATE a product
-route.put('/update/:id', multer.none(), authenticateToken, async(req,res) => {
+route.put('/update/:id', multer.none(), authenticateToken, async (req, res) => {
     try {
         const { id } = req.params;
         const { nama_produk, desc, kategori, qty, status, years_of_usage } = req.body;
 
         const product = await supabase.from('barang').select('nama_produk').eq('id', id);
-        if(product.data[0] == null){
-            return res.status(404).json({message: "Product with this id does not exist."})
+        if (product.data[0] == null) {
+            return res.status(404).json({ message: "Product with this id does not exist." })
         }
 
         const updatedProduct = await supabase
-        .from('barang')
-        .update({nama_produk: nama_produk, desc: desc, kategori: kategori, qty: qty, status: status, years_of_usage: years_of_usage })
-        .eq('id', id)
-        .select("*");
+            .from('barang')
+            .update({ nama_produk: nama_produk, desc: desc, kategori: kategori, qty: qty, status: status, years_of_usage: years_of_usage })
+            .eq('id', id)
+            .select("*");
 
         return res.status(200).send(updatedProduct.data);
 
@@ -89,17 +107,17 @@ route.put('/update/:id', multer.none(), authenticateToken, async(req,res) => {
 });
 
 //DELETE a product
-route.delete('/delete/:id', authenticateToken, async(req,res) => {
+route.delete('/delete/:id', authenticateToken, async (req, res) => {
     try {
         const { id } = req.params;
         const product = await supabase.from('barang').select('nama_produk').eq('id', id);
 
-        if(product.data[0] == null){
-            return res.status(404).json({message: "Product with this id does not exist."})
+        if (product.data[0] == null) {
+            return res.status(404).json({ message: "Product with this id does not exist." })
         }
 
         await supabase.from('barang').delete().eq('id', id);
-        return res.status(200).json({ message: "Delete success."});
+        return res.status(200).json({ message: "Delete success." });
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
